21db18489806a5ebf89a2d50e5644fd76259bdc7
hed3d <hed3da@gmail.com>
2026-05-07 10:29:09 +0300

    Update data.js

--- data.js
+++ data.js
@@ -4,51 +4,58 @@
 // Після редагування закомітьте файл в GitHub
 
 var DEFAULT_SCHEDULE = {
-  monday: {
-    1: {
-      time: "08:30 - 09:15",
-      name: "Математика",
-      link: "",
-      hasGroups: false,
-      group1Link: "",
-      group2Link: ""
+  "monday": {},
+  "tuesday": {},
+  "wednesday": {},
+  "thursday": {
+    "1": {
+      "time": "08:00–08:45",
+      "name": "Зарубіжна література",
+      "link": "https://meet.google.com/ecg-cxwe-oud",
+      "hasGroups": false,
+      "group1Link": "",
+      "group2Link": ""
     },
-    2: {
-      time: "09:30 - 10:15",
-      name: "Українська мова",
-      link: "",
-      hasGroups: false,
-      group1Link: "",
-      group2Link: ""
+    "2": {
+      "time": "08:50–09:35",
+      "name": "Біологія",
+      "link": "https://meet.google.com/bkb-xhph-vfx",
+      "hasGroups": false,
+      "group1Link": "",
+      "group2Link": ""
     },
-    3: {
-      time: "10:30 - 11:15",
-      name: "Англійська мова",
-      link: "",
-      hasGroups: true,
-      group1Link: "",
-      group2Link: ""
+    "3": {
+      "time": "09:45–10:30",
+      "name": "Математика",
+      "link": "https://meet.google.com/pbv-todf-wmq",
+      "hasGroups": false,
+      "group1Link": "",
+      "group2Link": ""
+    },
+    "4": {
+      "time": "10:40–11:25",
+      "name": "Математика",
+      "link": "https://meet.google.com/pbv-todf-wmq",
+      "hasGroups": false,
+      "group1Link": "",
+      "group2Link": ""
+    },
+    "5": {
+      "time": "11:35–12:20",
+      "name": "Українська мова",
+      "link": "",
+      "hasGroups": true,
+      "group1Link": "https://meet.google.com/syg-wkdi-bpz",
+      "group2Link": "https://meet.google.com/hpz-yadh-itq"
+    },
+    "6": {
+      "time": "12:25–13:10",
+      "name": "Українська мова",
+      "link": "",
+      "hasGroups": true,
+      "group1Link": "https://meet.google.com/syg-wkdi-bpz",
+      "group2Link": "https://meet.google.com/hpz-yadh-itq"
     }
   },
-  tuesday: {
-    1: {
-      time: "08:30 - 09:15",
-      name: "Фізика",
-      link: "",
-      hasGroups: false,
-      group1Link: "",
-      group2Link: ""
-    },
-    2: {
-      time: "09:30 - 10:15",
-      name: "Хімія",
-      link: "",
-      hasGroups: false,
-      group1Link: "",
-      group2Link: ""
-    }
-  },
-  wednesday: {},
-  thursday: {},
-  friday: {}
+  "friday": {}
 };
